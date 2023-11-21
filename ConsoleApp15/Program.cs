96a67c488fdffe62fe1ee02a5c82eca9f8c62236
visual studio <vs@itstep.ge>
2023-11-21 19:54:17 +0400

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] arr = { 1, 2, 3, 4, 5 };
+
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("-----------");
+            //for (int i = arr.Length-1; i >=0; i--)
+            //{
+            //    Console.Write(arr[i] + "");
+            //}
+
+            Student student = new Student()
+            {
+                Name = "john",
+                id = 1,
+                GPA = 3.5
+            };
+
+            Student student1 = new Student()
+            {
+                Name = "jane",
+                id = 2,
+                GPA = 3.2
+            };
+
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add(student.id, student);
+            hashtable.Add(student1.id, student1);
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            foreach(Student value in hashtable.Values)
+            {
+                if(choice == value.id)
+                {
+                    Console.WriteLine(value.Name);
+                    Console.WriteLine(value.GPA);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp15/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+        public double GPA { get; set; }
+    }
+}
